666b768d309d2c46c8c52a90ea4747c540158c34
Febin <jamesfebin@gmail.com>
2023-05-21 18:50:54 +0530

    Added github links

---
+++ _app/immutable/nodes/2.904be8a4.js
@@ -0,0 +1,17 @@
+import{S as G,i as L,s as J,k as n,a as M,q as m,l as r,m as h,c as V,h as i,r as f,J as K,n as c,p as w,b as Q,G as t,H as S}from"../chunks/index.394dbf96.js";const U=!0,Y=Object.freeze(Object.defineProperty({__proto__:null,prerender:U},Symbol.toStringTag,{value:"Module"}));function W(C){let o,v,a,l,d,q,x,E,j,g,e,T,k,A,P,p,F,N,u,O,R;return{c(){o=n("main"),v=n("center"),a=n("div"),l=n("div"),d=n("img"),x=M(),E=n("br"),j=M(),g=n("div"),e=n("p"),T=m(`Hello there! 👋 I'm Febin. I find joy in exploring emerging
+				technology and shaping products that people enjoy using. Over
+				the past decade, I've navigated through diverse fields including
+				engineering, design, and writing. Now, I'm fortunate to lead
+				Partnerships at Tars, where I assist businesses in modernizing
+				their customer support through the power of chatbots. 🤖
+				`),k=n("br"),A=n("br"),P=m(`
+				This blog is my playground 🎡 where I share my thoughts and experiments about the shape of future applications in the landscape of AI. 
+				Feel free to connect with me on  `),p=n("a"),F=m("LinkedIn"),N=m("  or "),u=n("a"),O=m("Twitter"),R=m("."),this.h()},l(_){o=r(_,"MAIN",{});var b=h(o);v=r(b,"CENTER",{});var z=h(v);a=r(z,"DIV",{class:!0,style:!0});var y=h(a);l=r(y,"DIV",{class:!0});var I=h(l);d=r(I,"IMG",{src:!0,style:!0}),x=V(I),E=r(I,"BR",{}),I.forEach(i),j=V(y),g=r(y,"DIV",{class:!0});var B=h(g);e=r(B,"P",{style:!0});var s=h(e);T=f(s,`Hello there! 👋 I'm Febin. I find joy in exploring emerging
+				technology and shaping products that people enjoy using. Over
+				the past decade, I've navigated through diverse fields including
+				engineering, design, and writing. Now, I'm fortunate to lead
+				Partnerships at Tars, where I assist businesses in modernizing
+				their customer support through the power of chatbots. 🤖
+				`),k=r(s,"BR",{}),A=r(s,"BR",{}),P=f(s,`
+				This blog is my playground 🎡 where I share my thoughts and experiments about the shape of future applications in the landscape of AI. 
+				Feel free to connect with me on  `),p=r(s,"A",{href:!0,target:!0});var D=h(p);F=f(D,"LinkedIn"),D.forEach(i),N=f(s,"  or "),u=r(s,"A",{href:!0,target:!0});var H=h(u);O=f(H,"Twitter"),H.forEach(i),R=f(s,"."),s.forEach(i),B.forEach(i),y.forEach(i),z.forEach(i),b.forEach(i),this.h()},h(){K(d.src,q="/febin.png")||c(d,"src",q),w(d,"height","200px",1),w(d,"border-radius","20px"),c(l,"class","column is-2"),c(p,"href","https://www.linkedin.com/in/febinjohnjames/"),c(p,"target","_blank"),c(u,"href","https://twitter.com/heyfebin"),c(u,"target","_blank"),w(e,"text-align","justify"),c(g,"class","column is-8"),c(a,"class","columns"),w(a,"padding","10px")},m(_,b){Q(_,o,b),t(o,v),t(v,a),t(a,l),t(l,d),t(l,x),t(l,E),t(a,j),t(a,g),t(g,e),t(e,T),t(e,k),t(e,A),t(e,P),t(e,p),t(p,F),t(e,N),t(e,u),t(u,O),t(e,R)},p:S,i:S,o:S,d(_){_&&i(o)}}}class Z extends G{constructor(o){super(),L(this,o,null,W,J,{})}}export{Z as component,Y as universal};
